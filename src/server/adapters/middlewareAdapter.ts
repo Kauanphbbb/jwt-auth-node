12e16a2c82c83a3ccb155c5599e9d7dc4e766a5b
kauanphbbb <kauanphbbb@gmail.com>
2024-10-09 19:10:17 -0300

    fix: correct middleware adapter

--- src/server/adapters/middlewareAdapter.ts
+++ src/server/adapters/middlewareAdapter.ts
@@ -2,20 +2,27 @@ import { NextFunction, Request, Response } from 'express';
 import { IMiddleware } from '../../app/interfaces/IMiddleware';
 
 export function middlewareAdapter(middleware: IMiddleware) {
-  return async (request: Request, response: Response, next: NextFunction) => {
-    const result = await middleware.handle({
-      headers: request.headers as Record<string, string>,
-    });
+  return async (
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    try {
+      const result = await middleware.handle({
+        headers: request.headers as Record<string, string>,
+      });
 
-    if ('statusCode' in result) {
-      return response.status(result.statusCode).json(result.body);
+      if ('statusCode' in result) {
+        response.status(result.statusCode).json(result.body); // Envia a resposta
+      } else {
+        request.metadata = {
+          ...request.metadata,
+          ...result.data,
+        };
+        next(); // Chama o próximo middleware
+      }
+    } catch (error) {
+      next(error); // Encaminha erros para o middleware de erro
     }
-
-    request.metadata = {
-      ...request.metadata,
-      ...result.data,
-    };
-
-    next();
   };
 }
