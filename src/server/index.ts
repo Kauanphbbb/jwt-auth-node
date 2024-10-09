c41be3941f769f9bf9ed9315e157a8ac725a10da
kauanphbbb <kauanphbbb@gmail.com>
2024-10-09 19:00:57 -0300

    feat: create a protected route

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import { env } from '../app/config/env';
+import { makeAuthenticationMiddleware } from '../factories/makeAuthenticationMiddleware';
+import { makeListLeadsController } from '../factories/makeListLeadsController';
 import { makeSignInController } from '../factories/makeSignInController';
 import { makeSignUpController } from '../factories/makeSignUpController';
+import { middlewareAdapter } from './adapters/middlewareAdapter';
 import { routeAdapter } from './adapters/routeAdapter';
 
 const app = express();
@@ -15,6 +18,12 @@ app.get('/', (req, res) => {
 app.post('/sign-up', routeAdapter(makeSignUpController()));
 
 app.post('/sign-in', routeAdapter(makeSignInController()));
+
+app.get(
+  '/leads',
+  middlewareAdapter(makeAuthenticationMiddleware()),
+  routeAdapter(makeListLeadsController())
+);
 
 app.listen(env.PORT, () => {
   console.log(`Server running on port ${env.PORT}`);
