d8b8eaf980209a845ea504279b3f9bb33adb5b53
kauanphbbb <kauanphbbb@gmail.com>
2024-10-09 17:00:32 -0300

    feat: create route adapter

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { env } from '../app/config/env';
 import { makeSignInController } from '../factories/makeSignInController';
 import { makeSignUpController } from '../factories/makeSignUpController';
+import { routeAdapter } from './adapters/routeAdapter';
 
 const app = express();
 
@@ -11,25 +12,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.post('/sign-up', async (req, res) => {
-  const signUpUseController = makeSignUpController();
+app.post('/sign-up', routeAdapter(makeSignUpController()));
 
-  const { statusCode, body } = await signUpUseController.handle({
-    body: req.body,
-  });
-
-  res.status(statusCode).json(body);
-});
-
-app.post('/sign-in', async (req, res) => {
-  const signInController = makeSignInController();
-
-  const { statusCode, body } = await signInController.handle({
-    body: req.body,
-  });
-
-  res.status(statusCode).json(body);
-});
+app.post('/sign-in', routeAdapter(makeSignInController()));
 
 app.listen(env.PORT, () => {
   console.log(`Server running on port ${env.PORT}`);
